6789c58faa9a3074b7fe4521415fe2bfb8c1a269
Danielle Barbosa <daniellebarbosa.ti81@gmail.com>
2025-03-02 18:19:30 -0300

    Projeto inicial blog-DIO

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "O Que É Tecnologia Na saúde? ",
+    "description": 
+    " A tecnologia na saúde nada mais é que todas as soluções desenvolvidas por meio da combinação entre as duas áreas." +
+
+"  Tal parceria existe há séculos, e ela é fundamental para a evolução que vem acontecendo em ambos os setores."+
+
+"  Isso se dá pois a saúde cria as demandas, e a tecnologia aprimora o seu aparato para suprir tais demandas."+
+
+"  Dessa forma, a tecnologia vai proporcionando diversos avanços, tanto na prestação de serviços, quanto em novos equipamentos."+
+
+"  E, devido a esses avanços tecnológicos, foi possível a humanidade desenvolver diversas técnicas para prevenção e combate de doenças que já provocaram muitas tragédias ao dizimar milhões de pessoas. ",
+
+    "photoCover":"https://th.bing.com/th/id/OIP.lbV9MAWh3EnKhoVcJTgU1wHaEc?w=254&h=184&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+
+  },
+
+]
